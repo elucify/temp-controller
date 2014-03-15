e1832353e18329303f195b6807c95d43370a16d8
Mark Johnson <elucify@gmail.com>
2014-03-15 11:24:31 -0400

    Refactored main into TempController, added OneWire sense, no ROM yet

--- TempController.cpp
+++ TempController.cpp
@@ -156,14 +156,157 @@ DisplayBoard::~DisplayBoard()
 //
 
 TempController::TempController(byte pinDin, byte pinClk, byte pinLoad, byte brightness, byte numDevices):
-  _db(pinDin, pinClk, pinLoad, brightness, numDevices)
+  _displayBoard(pinDin, pinClk, pinLoad, brightness, numDevices)
 {
     init();
 }
 
+void TempController::run()
+{
+    float a = _thermometers[CHANNEL_A]->getTemperature();
+    float b = _thermometers[CHANNEL_B]->getTemperature();
+
+    if (a > -100.0) {
+        temp_a = a;
+    } else {
+        Serial.println("A failed");
+    }
+    if (b > -100.0) {
+        temp_b = b;
+    } else {
+        Serial.println("b failed");
+    }
+  
+    float diff_a = temp_a - ref_temp_a;
+    float diff_b = temp_b - ref_temp_b;
+
+    Serial.println("");
+    Serial.print("a:");
+    Serial.print(temp_a);
+    Serial.print(":");
+    Serial.print(diff_a);
+    Serial.print(":b:");
+    Serial.print(temp_b);
+    Serial.print(":");
+    Serial.println(diff_b);
+  
+    updateDisplay(temp_a, temp_b,
+                  diff_a < (-1 * tolerance_a),
+                  diff_a > tolerance_a,
+                  diff_b < (-1 * tolerance_b),
+                  diff_b > tolerance_b);
+  
+    delay(2000);
+}
+
+// Read thermometers and write to ROM -or- read thermometer addresses from ROM
+void TempController::initializeThermometers()
+{
+    // TODO: READ ROM
+    DeviceAddress thermometer_address_a = { 0x28, 0xFC, 0x74, 0xC9, 0x02, 0x00, 0x00, 0x86 };
+    DeviceAddress thermometer_address_b = { 0x28, 0xDA, 0x25, 0x55, 0x05, 0x00, 0x00, 0x53};
+    
+
+    // Copy addresses to working memory
+    memcpy(_thermometer_address[CHANNEL_A], thermometer_address_a, 8);
+    memcpy(_thermometer_address[CHANNEL_B], thermometer_address_b, 8);
+
+    // Initialize thermometer objects
+    _thermometers[CHANNEL_A] = new Thermometer(_thermometer_address[CHANNEL_A]);
+    _thermometers[CHANNEL_B] = new Thermometer(_thermometer_address[CHANNEL_B]);
+
+    OneWire *wire = Thermometer::getOneWire();
+
+
+    // If both buttons down on power-up, read and
+    // report sensors
+    if (_displayBoard.isButtonPushed(BUTTON_UP) &&
+        _displayBoard.isButtonPushed(BUTTON_DOWN)) {
+
+        byte addr[8];
+        byte naddrs = 0;
+
+        Serial.println("> Scanning devices");
+
+        while(wire->search(addr)) {
+
+            naddrs++;
+
+            Serial.print("Device ");
+            Serial.println(naddrs);
+
+            if (OneWire::crc8(addr, 7) != addr[7]) {
+                Serial.print("INVALID: ");
+            }
+
+            for (byte i = 0; i < 8; i++) {
+                if (!(addr[i] & 0xf0)) {
+                    Serial.print("0");
+                }
+                Serial.print(addr[i], HEX);
+                Serial.print(i == 7 ? "\n" : " ");
+            }
+        }
+        wire->reset_search();
+
+        Serial.print("> Devices found: ");
+        Serial.println(naddrs);
+    }
+}
+
+// Initialize thermometers
 void TempController::init()
 {
-    // Nothing needed
+    initializeThermometers();
+
+    // Initialize internal states
+    state = STATE_WAIT_BUTTON_DOWN;
+    prevstate = 0xff;
+    count = 0;
+    holdcount = 1000 / DELAYTIME; // After 2 seconds, start counting fast
+    holdperiod = HOLDPERIOD0;
+    temp_a = temp_b = 0.0;
+    tolerance_a = tolerance_b = 0.5;
+    ref_temp_a = ref_temp_b = 48.0;
+
+    updateDisplay(temp_a, temp_b, false, false, false, false);    
+}
+
+void TempController::updateDisplay(float a, float b,
+   boolean cold_a = false,
+   boolean hot_a = false,
+   boolean cold_b = false,
+   boolean hot_b = false)
+{
+  _displayBoard.temp(CHANNEL_A, a);
+  _displayBoard.temp(CHANNEL_B, b);
+  
+  _displayBoard.hot(CHANNEL_A, hot_a);
+  _displayBoard.hot(CHANNEL_B, hot_b);
+
+  _displayBoard.cold(CHANNEL_A, cold_a);
+  _displayBoard.cold(CHANNEL_B, cold_b);
+}
+
+void TempController::printButtons()
+{
+    Serial.print("UP = ");
+    Serial.println(_displayBoard.isButtonPushed(BUTTON_UP));
+    Serial.print("DOWN = ");
+    Serial.println(_displayBoard.isButtonPushed(BUTTON_DOWN));
+    Serial.print("RIGHT = ");
+    
+    Serial.print("Switch is ");
+    if (_displayBoard.isSwitchRight()) {
+      Serial.println("RIGHT");
+    }
+    if (_displayBoard.isSwitchLeft()) {
+      Serial.println("LEFT");
+    }
+    
+    if (_displayBoard.isSwitchCenter()) {
+      Serial.println("CENTER");
+    }
 }
 
 TempController::~TempController()
--- TempController.h
+++ TempController.h
@@ -2,6 +2,7 @@
 #define __TEMPCONTROLLER_H_
 
 #include <Arduino.h>
+#include <Thermometer.h>
 #include "LedControl.h"
 
 // Now we need a LedControl to work with.
@@ -60,59 +61,102 @@
 #define CHANNEL_CLEAR ((byte)(0xff))
 #define CHANNEL_DASHES ((byte)(0xfe))
 
+// Initial number of ticks before count up/down when holding buttons
+#define HOLDPERIOD0 333
+
+// Time per loop (debounce)
+#define DELAYTIME 2
+
+#define STATE_READ 0
+#define STATE_WAIT_BUTTON_UP 1
+#define STATE_WAIT_BUTTON_DOWN 2
+#define STATE_COUNT_DOWN 3
+#define STATE_COUNT_UP 4
+
+#define WHICH_BUTTON_PUSHED (displayBoard.isButtonPushed(BUTTON_DOWN) ? "down" : (displayBoard.isButtonPushed(BUTTON_UP) ? "up" : "none"))
+
 //
 // DisplayBoard
 // Controls display and dispatches messages
 //
 class DisplayBoard {
 
-public:
+ public:
     //DisplayBoard();
-   DisplayBoard(byte pinDin = LC_DIN, byte pinClk = LC_CLK, byte pinLoad = LC_LOAD,
-                byte brightness = LC_BRIGHTNESS, byte numDevices = 1);
-   virtual ~DisplayBoard();
+    DisplayBoard(byte pinDin = LC_DIN, byte pinClk = LC_CLK, byte pinLoad = LC_LOAD,
+                 byte brightness = LC_BRIGHTNESS, byte numDevices = 1);
+    virtual ~DisplayBoard();
 
-   // Individual digit, led methods
-   void digit(byte digit, char value, boolean dp = false);
-   void bits(byte digit, byte value);
-   void led(byte which, boolean value);
-   void leds(byte value);
+    // Individual digit, led methods
+    void digit(byte digit, char value, boolean dp = false);
+    void bits(byte digit, byte value);
+    void led(byte which, boolean value);
+    void leds(byte value);
 
-   // Channel A, B methods
-   void temp(byte channel, byte value);
-   void temp(byte channe, float value);
-   void hot(byte channel, boolean value);
-   void cold(byte channel, boolean value);
+    // Channel A, B methods
+    void temp(byte channel, byte value);
+    void temp(byte channe, float value);
+    void hot(byte channel, boolean value);
+    void cold(byte channel, boolean value);
 
-   // Button/switch methods
-   boolean isButtonPushed(byte which = BUTTON_ANY);
+    // Button/switch methods
+    boolean isButtonPushed(byte which = BUTTON_ANY);
 
-   boolean isSwitchLeft();
-   boolean isSwitchRight();
-   boolean isSwitchCenter();
-   byte switchPosition();
+    boolean isSwitchLeft();
+    boolean isSwitchRight();
+    boolean isSwitchCenter();
+    byte switchPosition();
 
-protected:
-   LedControl _lc;
-   static const byte _ledlist[];
-   byte _leds_bits;
+ protected:
+    LedControl _lc;
+    static const byte _ledlist[];
+    byte _leds_bits;
 
-   void init(byte pinDin, byte pinClk, byte pinLoad,
-             byte brightness, byte numDevices);
+    void init(byte pinDin, byte pinClk, byte pinLoad,
+              byte brightness, byte numDevices);
 };
 
 // Doesn't yet do anything except initialize DisplayBoard
 // Probably DisplayBoard should be in a separate library
 class TempController {
-public:
+ public:
     TempController(byte pinDin = LC_DIN, byte pinClk = LC_CLK, byte pinLoad = LC_LOAD,
                    byte brightness = LC_BRIGHTNESS, byte numDevices = 1);
     void init();
+    void run();
+
+    void updateDisplay(float a, float b, boolean cold_a, boolean hot_a, boolean cold_b, boolean hot_b);
+    void printButtons();
     virtual ~TempController();
 
-protected:
-    DisplayBoard _db;
+ protected:
+    DisplayBoard _displayBoard;
+
+    DeviceAddress _thermometer_address[2];
+    Thermometer *_thermometers[2];
+
+    // button states
+    int count = 0;
+    int holdcount = 0;
+    int holdperiod = HOLDPERIOD0;
+    byte state, prevstate;
+
+    // Temperatures, tolerances and references
+    float temp_a, temp_b;
+    float tolerance_a, tolerance_b;
+    float ref_temp_a, ref_temp_b;
+
+    //
+    // protected methods
+    //
+    void initializeThermometers();
+
+};
+
+class LimitController {
+ public:
+    LimitController(uint8_t& limit);
+
 };
 
 #endif
-
